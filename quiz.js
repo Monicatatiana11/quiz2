6ba377cd3f66f80df5929b003b1c7c98e5491285
Monicatatiana11 <58921913+Monicatatiana11@users.noreply.github.com>
2020-12-11 21:40:32 -0500

    2

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ console.log("validador")
     var user = document.getElementById("user").value;
     var user = document.getElementById("pass")value;
 
-    if (user =="mail@address.com" & pass == "1234") {
+    if (user =="mail@address.com" && pass == "1234") {
         location.href = "https://www.youtube.com/?gl=CO&hl=es-419";
     }
 
