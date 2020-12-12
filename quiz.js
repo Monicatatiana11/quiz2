c6bdbca2fe985c5eaceb1cd79276963ce7093390
Monicatatiana11 <58921913+Monicatatiana11@users.noreply.github.com>
2020-12-11 21:06:17 -0500

    quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function validador (){
+
+console.log("validador")
+    var user = document.getElementById("user").value;
+    var user = document.getElementById("pass")value;
+
+    if (user =="mail@address.com" & pass == "1234") {
+        location.href = "https://www.youtube.com/?gl=CO&hl=es-419";
+    }
+
+else { document.getElementById ("validdor").innerHTML = "password correcto"
+}
+
+}
+
